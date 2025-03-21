cac2e8b74320e076f6f95b515b5067123403e327
Egor <egorchik.kobylkin@gmail.com>
2025-03-21 03:33:56 +0300

    Fixed movement, changed to player model

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -15,7 +15,7 @@ public class PlayerMovement : MonoBehaviour
 
     void Start()
     {
-        Cursor.lockState = CursorLockMode.Confined;
+        Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
         
         cm = Camera.main;
@@ -28,21 +28,37 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         Vector2 moveInput = moveAction.ReadValue<Vector2>();
-        Vector3 moveDir = new Vector3(moveInput.x, 0, moveInput.y);
+        //Vector3 moveDir = new Vector3(moveInput.x, 0, 0);
+        //moveDir = moveDir.normalized * moveSpeed * Time.deltaTime;
+        // if (!characterController.isGrounded)
+        // {
+        //     moveDir.y = -fallSpeed * Time.deltaTime;
+        // }
+        //Debug.DrawRay(transform.position, moveDir * 5, Color.magenta);
         
-
-        moveDir = moveDir.normalized * moveSpeed * Time.deltaTime;
-
+        Vector3 inputDir = new Vector3(moveInput.x, 0, moveInput.y);
+        Vector3 moveDirFixed = transform.rotation * inputDir;
+        moveDirFixed = moveDirFixed.normalized * moveSpeed * Time.deltaTime;
         if (!characterController.isGrounded)
         {
-            moveDir.y = -fallSpeed * Time.deltaTime;
+            moveDirFixed.y = -fallSpeed * Time.deltaTime;
+        }
+        Debug.DrawRay(transform.position, moveDirFixed * 5, Color.blue);
+
+
+        //Vector3 projectedVec = Vector3.Project(rotationVector, transform.forward);
+        //float angleToRot = Vector3.Angle(transform.forward, crossProd);
+        //Debug.Log(angleToRot);
+        Vector3 rotationVector = Quaternion.AngleAxis(90, Vector3.up) * (cm.transform.position - transform.position);
+        Vector3 crossProd = Vector3.Cross(transform.up, rotationVector);
+        if (Mathf.Abs(characterController.velocity.magnitude) > 0.5f) // блокировка вращения если velocity маленький
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(crossProd, Vector3.up), rotationSpeed * Time.deltaTime);
         }
 
-        Debug.DrawRay(transform.position, moveDir * 10, Color.green);
+        Debug.DrawRay(transform.position, crossProd * 4, Color.green);
+        Debug.DrawRay(cm.transform.position, cm.transform.forward * 4, Color.red);
 
-        Vector3 worldPoint = cm.ScreenToWorldPoint(Input.mousePosition);
-        Debug.DrawRay(cm.transform.position, worldPoint);
-
-        characterController.Move(moveDir);
+        characterController.Move(moveDirFixed);
     }
 }
